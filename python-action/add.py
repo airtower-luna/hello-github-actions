131a2af1d3481adb2ec83fb6b1f1e8ba5f412aa8
Fiona Klute <fiona.klute@gmx.de>
2023-05-05 23:49:48 +0200

    Remove "set-output" remnants

--- python-action/add.py
+++ python-action/add.py
@@ -4,4 +4,5 @@ nums = []
 for i in range(1,5):
     nums.append(int(os.environ.get(f'INPUT_NUM{i}')))
 
-print(f'::set-output name=sum::{sum(nums)}')
+with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(f'sum={sum(nums)}', file=fh)
